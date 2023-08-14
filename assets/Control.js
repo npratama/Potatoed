dc8dd4d4f6c901bac8be91457a1cd57d52b10960
Nino Pratama <prasetyo.nino14@gmail.com>
2023-08-14 11:46:36 +0000

    ? refactor control

--- assets/Control.js
+++ assets/Control.js
@@ -4,14 +4,15 @@ export class Control {
     control;
     key;
     input;
+    touch;
 
     init(control, key, input){
         this.control = control ;
         this.key = key;
         this.input = input;
-
+        
         this.key = this.input.keyboard.createCursorKeys();
-        this.input.addPointer(3);
+        this.touch = this.input.addPointer(3);
     }
 
     Movement(player, input){
@@ -20,17 +21,17 @@ export class Control {
         //if key UP pressed
         if(input.pointer1.isDown)
         {
-            if((this.input.pointer1.x < (Constants.BTN_RIGHT_X + Constants.BTN_RADIUS) && this.input.pointer1.x > (Constants.BTN_RIGHT_X - Constants.BTN_RADIUS) )){
+            if((touch.pointer1.x < (Constants.BTN_RIGHT_X + Constants.BTN_RADIUS) && touch.pointer1.x > (Constants.BTN_RIGHT_X - Constants.BTN_RADIUS) )){
                 player.setVelocityX(200);
                 player.anims.play('right', true);
-            } else if((this.input.pointer1.x < (Constants.BTN_LEFT_X + Constants.BTN_RADIUS) && this.input.pointer1.x > (Constants.BTN_LEFT_X - Constants.BTN_RADIUS) )){
+            } else if((touch.pointer1.x < (Constants.BTN_LEFT_X + Constants.BTN_RADIUS) && touch.pointer1.x > (Constants.BTN_LEFT_X - Constants.BTN_RADIUS) )){
                 player.setVelocityX(-200);
                 player.anims.play('left', true);
-            } else if((this.input.pointer1.x < (Constants.BTN_UP_X + Constants.BTN_RADIUS) && this.input.pointer1.x > (Constants.BTN_UP_X - Constants.BTN_RADIUS)) && player.body.touching.down){
+            } else if((touch.pointer1.x < (Constants.BTN_UP_X + Constants.BTN_RADIUS) && touch.pointer1.x > (Constants.BTN_UP_X - Constants.BTN_RADIUS)) && player.body.touching.down){
                 player.setVelocityY(-420);
             }
 
-            if((this.input.pointer2.x< (Constants.BTN_UP_X + Constants.BTN_RADIUS) && this.input.pointer2.x > (Constants.BTN_UP_X - Constants.BTN_RADIUS)) && player.body.touching.down){
+            if((touch.pointer2.x< (Constants.BTN_UP_X + Constants.BTN_RADIUS) && touch.pointer2.x > (Constants.BTN_UP_X - Constants.BTN_RADIUS)) && player.body.touching.down){
                 player.setVelocityY(-420);
             }
         } 
