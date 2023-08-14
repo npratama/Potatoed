7889f9afc5b8a41d2709b164497d42c6a5c3e729
Nino Pratama <prasetyo.nino14@gmail.com>
2023-08-14 07:50:39 +0000

    ? tweak on control

--- assets/Control.js
+++ assets/Control.js
@@ -1,17 +1,21 @@
 export class Control {
     control;
     cursors;
+    input;
 
     init(control, cursors, input){
         this.control = control ;
         this.cursors = cursors;
+        this.input = input;
 
-        this.cursors = input.keyboard.createCursorKeys();
-        input.addPointer(3);
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.input.addPointer(3);
     }
 
     Movement(player, input){
-        //controller
+        //define action
+
+        //if key UP pressed
         if(input.pointer1.isDown)
         {
             if((this.input.pointer1.x < (BTN_RIGHT_X + BTN_RADIUS) && this.input.pointer1.x > (BTN_RIGHT_X-BTN_RADIUS) )){
@@ -21,7 +25,7 @@ export class Control {
                 player.setVelocityX(-200);
                 player.anims.play('left', true);
             } else if((this.input.pointer1.x < (BTN_UP_X + BTN_RADIUS) && this.input.pointer1.x > (BTN_UP_X - BTN_RADIUS)) && player.body.touching.down){
-                player.setVelocityY(-900);
+                player.setVelocityY(-420);
             }
 
             if((this.input.pointer2.x< (BTN_UP_X + BTN_RADIUS) && this.input.pointer2.x > (BTN_UP_X - BTN_RADIUS)) && player.body.touching.down){
@@ -33,7 +37,7 @@ export class Control {
             input.pointer1.x=0;
             input.pointer2.x=0;
             player.setVelocityX(0);
-            player.anims.play('turn');
+            // player.anims.play('turn');
         }	
 
         if (this.cursors.left.isDown)
